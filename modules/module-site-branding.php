906349bd575161e40416a5ac902a9561d2460ab1
Jason Cauwels <troutacular@gmail.com>
2015-02-27 10:18:23 -0800

    Revert "init"
    This reverts commit 4cfe685f12d2e964bb26ffca611e626c2f332640.

--- modules/module-site-branding.php
+++
@@ -1,7 +0,0 @@
-<div class="site-branding">
-	<h1 class="brand-title"><a href="http://www.usc.edu"><?php _e('University of Southern California',''); ?></a></h1>
-	<h2 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h2>
-	<?php if ( (get_bloginfo('description') != "") ) { ?>
-		<h3 class="site-description"><?php bloginfo( 'description' ); ?></h3>
-	<?php } ?>
-</div><!-- .site-branding -->
--- modules/module-site-navigation.php
+++
@@ -1,7 +0,0 @@
-<nav id="site-navigation" class="site-navigation" role="navigation">
-	<h2 class="site-navigation-title menu-toggle"><?php _e( 'Main Navigation', '_starter' ); ?></h2>
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_starter' ); ?></a>
-	<a class="skip-link screen-reader-text" href="#secondary"><?php _e( 'Skip to secondary content', 'theme-starter' ); ?></a>
-	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-navigation-list' ) ); ?>
-	<?php get_search_form(); ?>
-</nav><!-- .site-navigation -->
