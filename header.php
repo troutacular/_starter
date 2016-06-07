f1b114a83ffbea897c02c78289442d59b1f255bb
Jason Cauwels <troutacular@gmail.com>
2016-06-07 08:13:47 -0700

    header template package notes

--- header.php
+++ header.php
@@ -1,4 +1,15 @@
-<!DOCTYPE html>
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package _starter
+ */
+
+?><!DOCTYPE html>
 
 <!--[if IE 9]>
 <html class="ie ie9" <?php language_attributes(); ?>>
