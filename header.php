688223bacfe5a660bcd8d1a384fcc7a8be594467
Jason Cauwels <troutacular@gmail.com>
2016-06-06 17:20:09 -0700

    header note update to php standard

--- header.php
+++ header.php
@@ -13,8 +13,8 @@
 <?php 
 	
 	/**
-		Fix for validation
-	**/
+	 * Fix for validation
+	 */
 	if ( isset( $_SERVER['HTTP_USER_AGENT'] ) && ( strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false ) ) {
 		header( 'X-UA-Compatible: IE=edge,chrome=1' );
 	}
