de843c5b742af18b28a384501004661a2b0cecf4
Jason Cauwels <troutacular@gmail.com>
2016-06-08 17:12:50 -0700

    rm errant php call at end

--- header.php
+++ header.php
@@ -21,15 +21,15 @@
 
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<?php 
-	
+<?php
+
 	/**
 	 * Fix for validation
 	 */
 	if ( isset( $_SERVER['HTTP_USER_AGENT'] ) && ( strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false ) ) {
 		header( 'X-UA-Compatible: IE=edge,chrome=1' );
 	}
-	
+
 ?>
 <?php global $noindex; if( $noindex ) { echo '	<meta name="robots" content="noindex"/>'; } ?>
 <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" />
@@ -37,20 +37,19 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 <?php wp_head(); ?>
-	
+
 </head>
 <body <?php body_class(); ?>>
 
 <div class="site-wrapper"><?php /* closes in footer.php */ ?>
-	
+
 	<header id="masthead" class="site-header" role="banner">
-		
+
 		<?php get_template_part('modules/module-site-branding'); ?>
 		<?php get_template_part('modules/module-site-navigation'); ?>
-		
+
 	</header><!-- #masthead -->
-	
+
 	<?php get_template_part('modules/module-breadcrumbs'); ?>
-	
+
 	<div id="content" class="site-content-wrapper"><?php /* closes in footer.php */ ?>
-	<?php _starter_post_image()
