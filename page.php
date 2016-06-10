dcb265eac11d9f30d80a3062e20502e8879db664
Jason Cauwels <troutacular@gmail.com>
2016-06-10 15:33:33 -0700

    wp cleanup on page.php

--- page.php
+++ page.php
@@ -1,16 +1,13 @@
 <?php
 /**
-	
-	The template for displaying all pages.
-	
-	This is the template that displays all pages by default.
-	Please note that this is the WordPress construct of pages
-	and that other 'pages' on your WordPress site will use a
-	different template.
-	
-	@package usc-starter
-
-**/
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default. Please note that
+ * this is the WordPress construct of pages and that other 'pages' on your
+ * WordPress site will use a different template.
+ *
+ * @package usc-starter
+ */
 
 get_header(); ?>
 
@@ -22,15 +19,15 @@ get_header(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 				<?php
-					
-					/** 
-						If comments are open or we have at least one comment, 
-						load up the comment template 
-					**/
+
+					/**
+					 * If comments are open or we have at least one comment,
+					 * load up the comment template.
+					 */
 					if ( comments_open() || '0' != get_comments_number() ) {
 						comments_template();
 					}
-					
+
 				?>
 
 			<?php } // end of the loop. ?>
