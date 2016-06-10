e1fa05ac784fea22b46bc9c6e9d6bd40e132384b
Jason Cauwels <troutacular@gmail.com>
2016-06-10 08:33:32 -0700

    is-localhost cleanup

--- inc/is-localhost.php
+++ inc/is-localhost.php
@@ -1,20 +1,17 @@
 <?php
-	// Check if we are in a local environment
-		
-		function is_localhost() {
-			
-			// set the array for testing the local environment
-			$whitelist = array( '127.0.0.1', '::1' );
-			
-			// check if the server is in the array
-			if ( in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ) {
-				
-				// this is a local environment
-				return true;
-				
-			}
-			
-		}
-		
-	// end
-?>
+/**
+ * Check for local environment.
+ *
+ * Used for development and maintenance through development environments
+ *
+ * @return boolean [description]
+ */
+function is_localhost() {
+
+	// set the array for testing the local environment
+	$whitelist = array( '127.0.0.1', '::1' );
+
+	// check if the server is in the array
+	return in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ? true : false;
+
+}
