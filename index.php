2f72b35ac338459feb929de5c0c01441e9248154
Jason Cauwels <troutacular@gmail.com>
2016-06-10 14:21:03 -0700

    index file clean up

--- index.php
+++ index.php
@@ -1,51 +1,46 @@
 <?php
 /**
-	
-	The main template file.
-	
-	This is the most generic template file in a WordPress theme
-	and one of the two required files for a theme (the other being style.css).
-	It is used to display a page when nothing more specific matches a query.
-	
-	E.g., it puts together the home page when no home.php file exists.
-	Learn more: http://codex.wordpress.org/Template_Hierarchy
-	
-	@package usc-starter
-
-**/
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ *
+ * E.g., it puts together the home page when no home.php file exists.
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package usc-starter
+ */
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) { ?>
+		<?php
+		if ( have_posts() ) {
+			while ( have_posts() ) { the_post();
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) { the_post(); ?>
 
-				<?php
-					/**
-						Include the Post-Format-specific template for the content.
-						If you want to override this in a child theme, then include a file
-						called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						
-					**/
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
+				/**
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
+			}
 
-			<?php } ?>
+			_starter_paging_nav();
 
-			<?php _starter_paging_nav(); ?>
+		} else {
 
-		<?php } else { ?>
+			get_template_part( 'template-parts/content', 'none' );
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php } ?>
+		}
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
