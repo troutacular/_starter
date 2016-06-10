41114139edec608db845a16323468df3b0fc9d31
Jason Cauwels <troutacular@gmail.com>
2016-06-10 15:37:48 -0700

    auto format file

--- inc/navigation-descriptions.php
+++ inc/navigation-descriptions.php
@@ -5,7 +5,7 @@
  *
  * @see    http://wordpress.stackexchange.com/q/14037/
  * @author toscho, http://toscho.de
- * 
+ *
  * usage: wp_nav_menu( array( 'walker' => new Description_Walker ));
  */
 class Description_Walker extends Walker_Nav_Menu
@@ -13,21 +13,20 @@ class Description_Walker extends Walker_Nav_Menu
     /**
      * Start the element output.
      *
-     * @param  string $output Passed by reference. Used to append additional content.
-     * @param  object $item   Menu item data object.
-     * @param  int $depth     Depth of menu item. May be used for padding.
-     * @param  array $args    Additional strings.
+     * @param string  $output Passed by reference. Used to append additional content.
+     * @param object  $item   Menu item data object.
+     * @param int     $depth  Depth of menu item. May be used for padding.
+     * @param array   $args   Additional strings.
      * @return void
      */
-    function start_el(&$output, $item, $depth=0, $args=array(), $id=0)
-    {
+    function start_el( &$output, $item, $depth=0, $args=array(), $id=0 ) {
         $classes     = empty ( $item->classes ) ? array () : (array) $item->classes;
 
         $class_names = join(
             ' '
-        ,   apply_filters(
+            ,   apply_filters(
                 'nav_menu_css_class'
-            ,   array_filter( $classes ), $item
+                ,   array_filter( $classes ), $item
             )
         );
 
@@ -67,12 +66,12 @@ class Description_Walker extends Walker_Nav_Menu
 
         // Since $output is called by reference we don't need to return anything.
         $output .= apply_filters(
-            'walker_nav_menu_start_el'
-        ,   $item_output
-        ,   $item
-        ,   $depth
-        ,   $args
-        ,	$id
+            'walker_nav_menu_start_el',
+            $item_output,
+            $item,
+            $depth,
+            $args,
+            $id
         );
     }
 }
