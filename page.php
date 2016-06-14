91aeb73b330db3b95838de93a827ad70b47338fb
Jason Cauwels <troutacular@gmail.com>
2016-06-14 09:40:28 -0700

    single page clean up

--- page.php
+++ page.php
@@ -13,27 +13,21 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		<?php while ( have_posts() ) {
+            the_post();
+            get_template_part( 'template-parts/content', 'page' );
 
-			<?php while ( have_posts() ) { the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php
-
-					/**
-					 * If comments are open or we have at least one comment,
-					 * load up the comment template.
-					 */
-					if ( comments_open() || '0' != get_comments_number() ) {
-						comments_template();
-					}
-
-				?>
-
-			<?php } // end of the loop. ?>
-
+            /**
+			 * If comments are open or we have at least one comment,
+			 * load up the comment template.
+			 */
+			if ( comments_open() || '0' != get_comments_number() ) {
+				comments_template();
+			}
+        } // end of the loop. ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
