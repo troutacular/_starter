a3c00bac281db70b1a56cfcb67bd7766d8ff18fb
Jason Cauwels <troutacular@gmail.com>
2016-06-14 09:40:19 -0700

    functions: page navigation clean up

--- functions.php
+++ functions.php
@@ -355,26 +355,26 @@ if ( ! function_exists( '_starter_paging_nav' ) ) {
      *
      * Display navigation to next/previous set of posts when applicable.
      *
-     * @global  $wp_query  WP Query
-     * @return  string      HTML output of sibling links
+     * @global  $wp_query   WordPress Query Object
+     * @return  string      HTML output of pagination links
      */
     function _starter_paging_nav() {
+
+        global $wp_query;
+
         // Don't print empty markup if there's only one page.
-        if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+        if ( $wp_query->max_num_pages < 2 ) {
             return;
         } ?>
-        <nav class="navigation paging-navigation" role="navigation">
+        <nav class="navigation navigation-paging" role="navigation">
             <h1 class="screen-reader-text"><?php _e( 'Posts navigation', '_starter' ); ?></h1>
-            <div class="nav-links">
-
-            <?php if ( get_next_posts_link() ) { ?>
+            <div class="nav-links"><?php
+            if ( get_next_posts_link() ) { ?>
                 <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', '_starter' ) ); ?></div>
             <?php } ?>
-
             <?php if ( get_previous_posts_link() ) { ?>
                 <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', '_starter' ) ); ?></div>
             <?php } ?>
-
             </div><!-- .nav-links -->
         </nav><!-- .navigation --><?php
 
@@ -750,9 +750,11 @@ if ( ! function_exists( '_starter_paging_nav' ) ) {
         function _starter_setup_author() {
             global $wp_query;
 
+
             if ( $wp_query->is_author() && isset( $wp_query->post ) ) {
                 $GLOBALS['authordata'] = get_userdata( $wp_query->post->post_author );
             }
+
         }
     // end
 
