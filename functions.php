ec96fd19f5066c6942ab3f491812d5a36f38871b
Jason Cauwels <troutacular@gmail.com>
2016-06-15 08:28:43 -0700

    author setup function clean up

--- functions.php
+++ functions.php
@@ -735,28 +735,27 @@ if ( ! function_exists( '_starter_paging_nav' ) ) {
 7.0 - Author
 --------------------------------------------------------------*/
 
-    // Sets the authordata global when viewing an author archive.
-        /*
-         * This provides backwards compatibility with
-         * http://core.trac.wordpress.org/changeset/25574
-         *
-         * It removes the need to call the_post() and rewind_posts() in an author
-         * template to print information about the author.
-         *
-         * @global WP_Query 	$wp_query 	WordPress Query object.
-         * @return void
-         */
-        add_action( 'wp', '_starter_setup_author' );
-        function _starter_setup_author() {
-            global $wp_query;
+    /**
+     * Sets the authordata global when viewing an author archive.
+     *
+     * This provides backwards compatibility with
+     * http://core.trac.wordpress.org/changeset/25574
+     *
+     * It removes the need to call the_post() and rewind_posts() in an author
+     * template to print information about the author.
+     *
+     * @global  object 	$wp_query       WordPress Query object.
+     * @return  void
+     */
+    function _starter_setup_author() {
 
+        global $wp_query;
 
-            if ( $wp_query->is_author() && isset( $wp_query->post ) ) {
-                $GLOBALS['authordata'] = get_userdata( $wp_query->post->post_author );
-            }
-
+        if ( $wp_query->is_author() && isset( $wp_query->post ) ) {
+            $GLOBALS['authordata'] = get_userdata( $wp_query->post->post_author );
         }
-    // end
+    }
+    add_action( 'wp', '_starter_setup_author' );
 
 /*--------------------------------------------------------------
 8.0 - Index/Home
