822039ac3b21024fdf6996434235064d5646a073
Jason Cauwels <troutacular@gmail.com>
2016-06-16 13:44:35 -0700

    rm author setup: global $authordata exists - this is redundant

--- functions.php
+++ functions.php
@@ -838,28 +838,6 @@ if ( ! function_exists( '_starter_posted_on' ) ) {
 11.2 - Author
 --------------------------------------------------------------*/
 
-    /**
-     * Sets the authordata global when viewing an author archive.
-     *
-     * This provides backwards compatibility with
-     * http://core.trac.wordpress.org/changeset/25574
-     *
-     * It removes the need to call the_post() and rewind_posts() in an author
-     * template to print information about the author.
-     *
-     * @global  object 	$wp_query       WordPress Query object.
-     * @return  void
-     */
-    function _starter_setup_author() {
-
-        global $wp_query;
-
-        if ( $wp_query->is_author() && isset( $wp_query->post ) ) {
-            $GLOBALS['authordata'] = get_userdata( $wp_query->post->post_author );
-        }
-    }
-    add_action( 'wp', '_starter_setup_author' );
-
 
 /*--------------------------------------------------------------
 11.3 - Home
