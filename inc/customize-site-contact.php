c0da0ea26240f2a4c3af89d046cee97731247f6f
Jason Cauwels <troutacular@gmail.com>
2016-06-27 11:13:15 -0700

    cleanup phpmd/cs site contact

--- inc/customize-site-contact.php
+++ inc/customize-site-contact.php
@@ -13,7 +13,7 @@
  * @param  object 	$wp_customize 	WP Class for the Customizer
  * @return object
  */
-function _starter_site_contact_register($wp_customize) {
+function _starter_site_contact_register( $wp_customize ) {
 
 	// ----------------
 	//  Feedback Section
@@ -21,8 +21,8 @@ function _starter_site_contact_register($wp_customize) {
 	$wp_customize->add_section(
 		'_starter_feedback',
 		array(
-			'title'    => __('Feedback', '_starter'),
-			'priority' => 130
+			'title'    => __( 'Feedback', '_starter' ),
+			'priority' => 130,
 		)
 	);
 
@@ -35,16 +35,16 @@ function _starter_site_contact_register($wp_customize) {
 			'default'           => '',
 			'capability'        => 'edit_theme_options',
 			'type'              => 'option',
-			'sanitize_callback' => 'sanitize_text_field'
+			'sanitize_callback' => 'sanitize_text_field',
 		)
 	);
 
 	$wp_customize->add_control(
 		'_starter_text_feedback',
 		array(
-			'label'    => __('Website Feedback Text', '_starter'),
+			'label'    => __( 'Website Feedback Text', '_starter' ),
 			'section'  => '_starter_feedback',
-			'settings' => '_starter_options[text_feedback]'
+			'settings' => '_starter_options[text_feedback]',
 		)
 	);
 
@@ -57,16 +57,16 @@ function _starter_site_contact_register($wp_customize) {
 			'default'           => '',
 			'capability'        => 'edit_theme_options',
 			'type'              => 'option',
-			'sanitize_callback' => 'esc_url'
+			'sanitize_callback' => 'esc_url',
 		)
 	);
 
 	$wp_customize->add_control(
 		'_starter_text_feedback_link',
 		array(
-			'label'    => __('Website Feedback Link', '_starter'),
+			'label'    => __( 'Website Feedback Link', '_starter' ),
 			'section'  => '_starter_feedback',
-			'settings' => '_starter_options[text_feedback_link]'
+			'settings' => '_starter_options[text_feedback_link]',
 		)
 	);
 
@@ -79,16 +79,16 @@ function _starter_site_contact_register($wp_customize) {
 			'default'           => '',
 			'capability'        => 'edit_theme_options',
 			'type'              => 'option',
-			'sanitize_callback' => 'sanitize_text_field'
+			'sanitize_callback' => 'sanitize_text_field',
 		)
 	);
 
 	$wp_customize->add_control(
 		'_starter_text_feedback_link_text',
 		array(
-			'label'    => __('Website Feedback Link Text', '_starter'),
+			'label'    => __( 'Website Feedback Link Text', '_starter' ),
 			'section'  => '_starter_feedback',
-			'settings' => '_starter_options[text_feedback_link_text]'
+			'settings' => '_starter_options[text_feedback_link_text]',
 		)
 	);
 
@@ -98,8 +98,8 @@ function _starter_site_contact_register($wp_customize) {
 	$wp_customize->add_section(
 		'_starter_website_by',
 		array(
-			'title'    => __('Website By', '_starter'),
-			'priority' => 140
+			'title'    => __( 'Website By', '_starter' ),
+			'priority' => 140,
 		)
 	);
 
@@ -112,16 +112,16 @@ function _starter_site_contact_register($wp_customize) {
 			'default'           => '',
 			'capability'        => 'edit_theme_options',
 			'type'              => 'option',
-			'sanitize_callback' => 'sanitize_text_field'
+			'sanitize_callback' => 'sanitize_text_field',
 		)
 	);
 
 	$wp_customize->add_control(
 		'_starter_text_website_by',
 		array(
-			'label'    => __('Website By Text', '_starter'),
+			'label'    => __( 'Website By Text', '_starter' ),
 			'section'  => '_starter_website_by',
-			'settings' => '_starter_options[text_website_by]'
+			'settings' => '_starter_options[text_website_by]',
 		)
 	);
 
@@ -134,16 +134,16 @@ function _starter_site_contact_register($wp_customize) {
 			'default'           => '',
 			'capability'        => 'edit_theme_options',
 			'type'              => 'option',
-			'sanitize_callback' => 'esc_url'
+			'sanitize_callback' => 'esc_url',
 		)
 	);
 
 	$wp_customize->add_control(
 		'_starter_text_website_by_link',
 		array(
-			'label'    => __('Website By Link', '_starter'),
+			'label'    => __( 'Website By Link', '_starter' ),
 			'section'  => '_starter_website_by',
-			'settings' => '_starter_options[text_website_by_link]'
+			'settings' => '_starter_options[text_website_by_link]',
 		)
 	);
 
@@ -156,19 +156,19 @@ function _starter_site_contact_register($wp_customize) {
 			'default'           => '',
 			'capability'        => 'edit_theme_options',
 			'type'              => 'option',
-			'sanitize_callback' => 'sanitize_text_field'
+			'sanitize_callback' => 'sanitize_text_field',
 		)
 	);
 
 	$wp_customize->add_control(
 		'_starter_text_website_by_link_text',
 		array(
-			'label'    => __('Website By Link Text', '_starter'),
+			'label'    => __( 'Website By Link Text', '_starter' ),
 			'section'  => '_starter_website_by',
-			'settings' => '_starter_options[text_website_by_link_text]'
+			'settings' => '_starter_options[text_website_by_link_text]',
 		)
 	);
 }
 
 // add the function to the customize register
-add_action('customize_register', '_starter_site_contact_register');
+add_action( 'customize_register', '_starter_site_contact_register' );
