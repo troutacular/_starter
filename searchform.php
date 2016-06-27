322f57e96d15d069b02903bdf7b9f3eeed2c75c6
Jason Cauwels <troutacular@gmail.com>
2016-06-27 14:04:28 -0700

    search form phpmd/cs

--- searchform.php
+++ searchform.php
@@ -1,14 +1,11 @@
 <?php
 /**
-
-	The Search Form
-	
-	@package usc-starter
-
-**/
+ * The Search Form
+ *
+ * @package _starter
+ */
 ?>
-
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<input type="text" class="search-field" name="s" placeholder="search" title="search" />
-	<button type="submit" class="search-submit"><?php _e('Search', '_starter'); ?></button>
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<input type="text" class="search-field" name="s" placeholder="Search" title="search" />
+	<button type="submit" class="search-submit"><?php esc_html__( 'Search', '_starter' ); ?></button>
 </form>
