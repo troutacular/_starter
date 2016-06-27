fcb649f15d1344225c0b3dfae016c72ca17bc34a
Jason Cauwels <troutacular@gmail.com>
2016-06-27 13:54:35 -0700

    single post phpmd/cs

--- single.php
+++ single.php
@@ -1,39 +1,38 @@
 <?php
 /**
-
-	The Template for displaying all single posts.
-	
-	@package usc-starter
-
-**/
+ * The template for displaying all single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-pos
+ *
+ * @package _starter
+ */
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) { the_post(); ?>
+		<?php
+		while ( have_posts() ) {
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			// Setup the post data.
+			the_post();
 
-			<?php _starter_post_nav(); ?>
+			get_template_part( 'template-parts/content', 'single' );
 
-			<?php
-				
-				/**
-					If comments are open or we have at least one comment, 
-					load up the comment template
-				**/
-				if ( comments_open() || '0' != get_comments_number() ) {
-					comments_template();
-				}
-				
-			?>
+			// Post navigation.
+			_starter_post_nav();
 
-		<?php } // end of the loop. ?>
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || '0' !== get_comments_number() ) {
+				comments_template();
+			}
+		} // end of the loop.
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
