5f213a5f0eb59096d18581cfb834e80e7d5aabbc
Jason Cauwels <troutacular@gmail.com>
2016-06-27 13:54:44 -0700

    page phpmd/cs

--- page.php
+++ page.php
@@ -2,29 +2,36 @@
 /**
  * The template for displaying all pages.
  *
- * This is the template that displays all pages by default. Please note that
- * this is the WordPress construct of pages and that other 'pages' on your
- * WordPress site will use a different template.
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
- * @package usc-starter
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _starter
  */
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php while ( have_posts() ) {
-            the_post();
-            get_template_part( 'template-parts/content', 'page' );
 
-            /**
-			 * If comments are open or we have at least one comment,
-			 * load up the comment template.
-			 */
-			if ( comments_open() || '0' != get_comments_number() ) {
+		<?php
+		while ( have_posts() ) {
+
+			// Setup the post data.
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || '0' !== get_comments_number() ) {
 				comments_template();
 			}
-        } // end of the loop. ?>
+		} // end of the loop.
+		?>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
