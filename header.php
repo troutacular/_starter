e4df5d3a1884d05d2f674720d8074753efcd15d6
Jason Cauwels <troutacular@gmail.com>
2016-06-29 09:50:05 -0700

    separate and comments header sections

--- header.php
+++ header.php
@@ -18,19 +18,20 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php
-// Display a noindex meta tag if required by the blog configuration.
-if ( function_exists( 'noindex' ) ) {
-	noindex();
-}
-?>
+	<?php /* meta information */ ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php
+	// Display a noindex meta tag if required by the blog configuration.
+	if ( function_exists( 'noindex' ) ) {
+		noindex();
+	}
+	?>
+	<?php /* link information */ ?>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 
 </head>
 <body <?php body_class(); ?>>
