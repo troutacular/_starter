3b93ca17a68a7a60586bfd97826ce06f283c1bbb
Jason Cauwels <troutacular@gmail.com>
2016-06-30 08:46:01 -0700

    move skip links to top of the page for accessibility

--- modules/module-site-navigation.php
+++ modules/module-site-navigation.php
@@ -1,8 +1,6 @@
 <nav id="site-navigation" class="site-navigation" role="navigation">
 	<?php // @todo - add aria expanded support for menu ?>
 	<h2 class="site-navigation-title menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Main Navigation', '_starter' ); ?></h2>
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_starter' ); ?></a>
-	<a class="skip-link screen-reader-text" href="#secondary"><?php esc_html_e( 'Skip to secondary content', '_starter' ); ?></a>
 	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-navigation-list' ) ); ?>
 	<?php get_search_form(); ?>
 </nav><!-- .site-navigation -->
