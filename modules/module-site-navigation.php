11226c0d574f50bd7b748fc06ece7cdc18c90822
Jason Cauwels <troutacular@gmail.com>
2016-06-30 08:47:08 -0700

    site navigation template clean up

--- modules/module-site-navigation.php
+++ modules/module-site-navigation.php
@@ -1,6 +1,13 @@
+<?php
+/**
+ * The template for displaying the Primary Navigation.
+ *
+ * @package _starter
+ */
+
+?>
 <nav id="site-navigation" class="site-navigation" role="navigation">
-	<?php // @todo - add aria expanded support for menu ?>
-	<h2 class="site-navigation-title menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Main Navigation', '_starter' ); ?></h2>
-	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-navigation-list' ) ); ?>
-	<?php get_search_form(); ?>
+	<?php // @todo - add aria expanded support for menu. ?>
+	<h2 class="site-navigation-title menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_starter' ); ?></h2>
+	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu-id' => 'primary-menu', 'menu_class' => 'site-navigation-list' ) ); ?>
 </nav><!-- .site-navigation -->
