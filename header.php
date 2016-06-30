f94307c816b51b8fd5fcf24831021db34049e8eb
Jason Cauwels <troutacular@gmail.com>
2016-06-30 10:52:57 -0700

    header inline site navigation and remove sub module

--- header.php
+++ header.php
@@ -41,7 +41,10 @@ if ( function_exists( 'noindex' ) ) {
 			<?php } ?>
 		</div><!-- .site-branding -->
 
-		<?php get_template_part( 'modules/module-site-navigation' ); ?>
+		<nav id="site-navigation" class="site-navigation" role="navigation">
+			<h2 class="site-navigation-title"><?php esc_html_e( 'Primary Menu', '_starter' ); ?></h2>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu-id' => 'primary-menu', 'menu_class' => 'site-navigation-list' ) ); ?>
+		</nav><!-- .site-navigation -->
 
 	</header><!-- #masthead -->
 
