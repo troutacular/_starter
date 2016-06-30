b9d1da84b7be06ec8d4269f7258f565620de64a3
Jason Cauwels <troutacular@gmail.com>
2016-06-30 08:32:16 -0700

    remove IE specific functions - should add only when needed

--- header.php
+++ header.php
@@ -10,29 +10,20 @@
  */
 
 ?><!DOCTYPE html>
-
-<!--[if IE 9]>
-<html class="ie ie9" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 9)  ]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
-	<?php /* meta information */ ?>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php
-	// Display a noindex meta tag if required by the blog configuration.
-	if ( function_exists( 'noindex' ) ) {
-		noindex();
-	}
-	?>
-	<?php /* link information */ ?>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+// Display a noindex meta tag if required by the blog configuration.
+if ( function_exists( 'noindex' ) ) {
+	noindex();
+}
+?>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-	<?php wp_head(); ?>
-
+<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
