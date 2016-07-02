80db2e4e9b7dffdab74d49172d133afcc711a7ac
Jason Cauwels <troutacular@gmail.com>
2016-07-02 07:27:59 -0700

    update content-none: escapes, phpmd/cs

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,35 +1,37 @@
 <?php
 /**
-	
-	The template part for displaying a message that posts cannot be found.
-	
-	Learn more: http://codex.wordpress.org/Template_Hierarchy
-	
-	@package usc-starter
-	
-**/
+ * Template part for displaying a message that posts cannot be found.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _starter
+ */
+
 ?>
 
-<section class="no-results">
+<section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', '_starter' ); ?></h1>
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', '_starter' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) { ?>
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', '_starter' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', '_starter' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
-		<?php } elseif ( is_search() ) { ?>
+		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '_starter' ); ?></p>
-			<?php get_search_form(); ?>
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '_starter' ); ?></p>
+			<?php
+				get_search_form();
 
-		<?php } else { ?>
+		else : ?>
 
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', '_starter' ); ?></p>
-			<?php get_search_form(); ?>
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', '_starter' ); ?></p>
+			<?php
+				get_search_form();
 
-		<?php } ?>
+		endif; ?>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
