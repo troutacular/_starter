1ebc4b2449326d7b6ad430794be2c2983bd1bfc5
Jason Cauwels <troutacular@gmail.com>
2016-07-05 08:38:50 -0700

    search form phpcs/md

--- searchform.php
+++ searchform.php
@@ -4,6 +4,7 @@
  *
  * @package _starter
  */
+
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<input type="text" class="search-field" name="s" placeholder="Search" title="search" />
