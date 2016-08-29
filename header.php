0cd4bbffcfedfb5a00b35fa201b616351b2fbc59
Jason Cauwels <troutacular@gmail.com>
2016-08-29 14:02:48 -0700

    site navigation menu updates

--- header.php
+++ header.php
@@ -43,7 +43,7 @@ if ( function_exists( 'noindex' ) ) {
 
 		<nav id="site-navigation" class="site-navigation" role="navigation">
 			<h2 class="site-navigation-title"><?php esc_html_e( 'Primary Menu', '_starter' ); ?></h2>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu-id' => 'primary-menu', 'menu_class' => 'site-navigation-list' ) ); ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu-id' => 'primary-menu', 'menu_class' => 'site-navigation-menu' ) ); ?>
 		</nav><!-- .site-navigation -->
 
 	</header><!-- #masthead -->
