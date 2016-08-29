db4c50b13ea64e807aa8c87e71e78f9f28e21c52
Jason Cauwels <troutacular@gmail.com>
2016-08-29 15:19:48 -0700

    correct search form text output

--- searchform.php
+++ searchform.php
@@ -7,6 +7,6 @@
 
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="text" class="search-field" name="s" placeholder="<?php esc_html__( 'Search', '_starter' ); ?>" title="search" />
-	<button type="submit" class="search-submit"><?php esc_html__( 'Search', '_starter' ); ?></button>
+	<input type="text" class="search-field" name="s" placeholder="<?php esc_html_e( 'Search', '_starter' ); ?>" title="search" />
+	<button type="submit" class="search-submit"><?php esc_html_e( 'Search', '_starter' ); ?></button>
 </form>
