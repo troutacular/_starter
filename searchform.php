cdb7576c2d6176a657fb0bd4b570deb38b9faeda
Jason Cauwels <troutacular@gmail.com>
2016-10-13 21:29:51 -0700

    search form label wrapper for screen readers

--- searchform.php
+++ searchform.php
@@ -7,6 +7,9 @@
 
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="text" class="search-field" name="s" placeholder="<?php esc_html_e( 'Search', '_starter' ); ?>" title="search" />
+	<label>
+		<span class="screen-reader-text"><?php esc_html_e( 'Search for:', '_starter' ); ?></span>
+		<input type="text" class="search-field" name="s" placeholder="<?php esc_html_e( 'Search', '_starter' ); ?>" title="search" />
+	</label>
 	<button type="submit" class="search-submit"><?php esc_html_e( 'Search', '_starter' ); ?></button>
 </form>
