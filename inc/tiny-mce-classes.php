1f642bce33b94f1ccc1bc141ed589ffcc3dd4c05
Jason Cauwels <troutacular@gmail.com>
2017-01-30 08:48:29 -0800

    add output of icon classes to tiny mce editor under 'formats'

--- inc/tiny-mce-classes.php
+++ inc/tiny-mce-classes.php
@@ -33,6 +33,36 @@ if ( ! function_exists( '_starter_tiny_mce_buttons' ) ) {
 	}
 }
 
+/**
+ * Add icons as style seclection to Tiny MCE editor.
+ *
+ * @since   3.1.0
+ * @param   string $type  Choose [icon] or [icon-only] list to include.
+ * @return  Array         Array of icons in WP Tiny MCE class format.
+ */
+function _starter_tiny_mce_icon_classes( $type = 'icon' ) {
+
+	if ( function_exists( '_starter_sprite_classes' ) ) {
+		// Get the icons.
+		$icons = _starter_sprite_classes();
+
+		// Set array for gathering classes.
+		$classes = array();
+
+		if ( ! empty( $icons[ $type ] ) ) {
+			foreach ( $icons[ $type ] as $icon ) {
+				$classes[] = array(
+					'title' => $icon,
+					'inline' => 'span',
+					'classes' => $icon,
+					'exact' => true,
+				);
+			}
+		}
+	}
+	return $classes;
+}
+
 // Register function callback to tiny_mce_before_init.
 add_filter( 'tiny_mce_before_init', '_starter_tiny_mce_before_init_insert_formats' );
 if ( ! function_exists( '_starter_tiny_mce_before_init_insert_formats' ) ) {
@@ -40,15 +70,25 @@ if ( ! function_exists( '_starter_tiny_mce_before_init_insert_formats' ) ) {
 	 * Callback function to filter the MCE settings.
 	 *
 	 * @since   3.1.0
+	 * @link    https://codex.wordpress.org/TinyMCE_Custom_Styles
 	 * @param   array $init_array  Array of editor settings.
 	 * @return  array              Updated array of editor settings.
 	 */
 	function _starter_tiny_mce_before_init_insert_formats( $init_array ) {
 
-		// Define the style_formats array.
-		$style_formats = array(
+		// Get icon list.
+		$icon = _starter_tiny_mce_icon_classes( 'icon' );
 
-			// Each array child is a format with it's own settings.
+		// Get icon only list.
+		$icon_only = _starter_tiny_mce_icon_classes( 'icon-only' );
+
+		// Merge icon lists.
+		$icon_formats = array_merge( $icon, $icon_only );
+
+		// Define the style_formats array.
+		$custom_formats = array(
+
+			// Insert custom child array formats.
 			array(
 				'title' => 'Horizontal Rule',
 				'block' => 'hr',
@@ -66,6 +106,9 @@ if ( ! function_exists( '_starter_tiny_mce_before_init_insert_formats' ) ) {
 				'wrapper' => 'div',
 			),
 		);
+
+		// Merge icon and custom style lists.
+		$style_formats = array_merge( $custom_formats, $icon_formats );
 
 		// Insert the json encoded array into 'style_formats'.
 		$init_array['style_formats'] = wp_json_encode( $style_formats );
