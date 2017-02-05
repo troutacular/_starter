7a68ba167b1e8194f07cdba270b7acf4754305d8
Jason Cauwels <troutacular@gmail.com>
2017-02-05 08:12:24 -0800

    packaged 3.3.0

--- inc/config-paths.php
+++ inc/config-paths.php
@@ -14,7 +14,7 @@ function _starter_get_config() {
 
 	// Set the default project configurations.
 	$config = array(
-		'version' => '3.2.0',
+		'version' => '3.3.0',
 		'assets' => array(
 			'filename_base' => '_starter',
 		),
