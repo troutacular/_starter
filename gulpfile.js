1b0c51f9972f5c322df76d2038684927ee34da57
Jason Cauwels <cauwels@usc.edu>
2017-07-27 10:18:47 -0700

    version project to 4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -108,7 +108,7 @@
 		},
 		// This section provides the information for the 'style.css' file in the root of the theme.
 		theme: {
-			version: '3.5.2',
+			version: '4.0.0',
 			name: 'Starter',
 			uri: 'https://github.com/troutacular/_starter',
 			author: '@troutacular',
